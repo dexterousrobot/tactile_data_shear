6f8c369023ed98abff0963fe66bb86d184944414
Nathan Lepora <n.lepora@bristol.ac.uk>
2023-03-28 11:57:26 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+import os
+from setuptools import setup
+
+lib_folder = os.path.dirname(os.path.realpath(__file__))
+
+# get required packages from requirements.txt
+requirement_path = os.path.join(lib_folder, 'requirements.txt')
+install_requires = []
+if os.path.isfile(requirement_path):
+    with open(requirement_path) as f:
+        install_requires = f.read().splitlines()
+
+setup(name='tactile_data_john',
+      version='0.0.1',
+      description='Storage of tactile data.',
+      author='John Lloyd, Nathan Lepora',
+      author_email='j.lloyd@bristol.ac.uk, n.lepora@bristol.ac.uk,',
+      license='',
+      packages=['tactile_data_john'],
+      install_requires=install_requires,
+      zip_safe=False)
---
+++ tactile_data_john/tactile_servo_control.py
@@ -0,0 +1,6 @@
+import os
+
+BASE_PATH = os.path.dirname(__file__)
+
+BASE_DATA_PATH = os.path.join(BASE_PATH, '..', 'data')
+BASE_MODEL_PATH = os.path.join(BASE_PATH, '..', 'models')
