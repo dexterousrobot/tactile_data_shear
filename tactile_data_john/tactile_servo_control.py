516ed53e2234434033774b554bc074dc555e3fd1
Nathan Lepora <n.lepora@bristol.ac.uk>
2023-04-01 18:07:35 +0100

    separation into surface_3d, surface_3d_pose, surface_3d_shear

--- tactile_data_john/tactile_servo_control.py
+++ tactile_data_john/tactile_servo_control.py
@@ -4,3 +4,4 @@ BASE_PATH = os.path.dirname(__file__)
 
 BASE_DATA_PATH = os.path.join(BASE_PATH, '..', 'data')
 BASE_MODEL_PATH = os.path.join(BASE_PATH, '..', 'models')
+BASE_RUNS_PATH = os.path.join(BASE_PATH, '..', 'runs')
